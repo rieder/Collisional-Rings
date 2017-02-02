95c8b7439e8ed7a875e98851930c0f62f5586fea
Steven Rieder <steven@rieder.nl>
2017-02-02 16:22:12 +0900

    Add script to build specific version of AMUSE that was used

--- get_amuse.sh
+++ get_amuse.sh
@@ -1,2 +1,21 @@
 #! /bin/bash
-wget https://github.com/amusecode/amuse/archive/036fe8562c63145d4546dc9f9eefe948b4a89f67.zip
+
+# Download and extract AMUSE
+wget https://github.com/amusecode/amuse/archive/036fe8562c63145d4546dc9f9eefe948b4a89f67.zip && \
+unzip 036fe8562c63145d4546dc9f9eefe948b4a89f67.zip && \
+mv amuse-036fe8562c63145d4546dc9f9eefe948b4a89f67 amuse && \
+rm 036fe8562c63145d4546dc9f9eefe948b4a89f67.zip 
+
+# Patch rebound makefile to compile on my Macbook
+cd amuse/src/amuse/community/rebound/
+python download_http.py 
+cd ../../../../../
+cp patches/rebound_Makefile.defs amuse/src/amuse/community/rebound/src/rebound/src/Makefile.defs 
+
+# Build AMUSE
+cd amuse/
+./configure && \
+make framework && \
+make rebound.code
+
+cd ..
