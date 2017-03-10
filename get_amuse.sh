1194fea2f671d35646e760535d8c618c2fc22b77
Steven Rieder <steven@rieder.nl>
2017-03-10 11:46:11 +0900

    Update to newer Amuse/Rebound

--- get_amuse.sh
+++ get_amuse.sh
@@ -1,24 +1,25 @@
 #! /bin/bash
 
-# Download and extract AMUSE
-wget https://github.com/amusecode/amuse/archive/036fe8562c63145d4546dc9f9eefe948b4a89f67.zip && \
-unzip 036fe8562c63145d4546dc9f9eefe948b4a89f67.zip && \
-mv amuse-036fe8562c63145d4546dc9f9eefe948b4a89f67 amuse && \
-rm 036fe8562c63145d4546dc9f9eefe948b4a89f67.zip 
+# Download and extract AMUSEa
+export GITHASH=0571e33c01fc380e9c614b19c0fa16aaa7696ea0
+wget https://github.com/rieder/amuse/archive/${GITHASH}.zip && \
+  unzip ${GITHASH}.zip && \
+  mv amuse-${GITHASH} amuse && \
+  rm ${GITHASH}.zip 
 
-export SCRIPT_DIR="$(PWD)"
-export AMUSE_DIR="$(PWD)/amuse"
-export REBOUND_DIR="$(AMUSE_DIR)/src/amuse/community/rebound"
+export SCRIPT_DIR="${PWD}"
+export AMUSE_DIR="${PWD}/amuse"
+export REBOUND_DIR="${AMUSE_DIR}/src/amuse/community/rebound"
 
 # Patch rebound makefile to compile on my Macbook
-cd $(REBOUND_DIR)
+cd ${REBOUND_DIR}
 python download_http.py 
-cd $(SCRIPT_DIR)
-cp patches/rebound_Makefile.defs $(REBOUND_DIR)/src/rebound/src/Makefile.defs 
-cp patches/rebound_interface.cc $(REBOUND_DIR)/interface.cc
+cd ${SCRIPT_DIR}
+cp patches/rebound_Makefile.defs ${REBOUND_DIR}/src/rebound/src/Makefile.defs 
+cp patches/rebound_interface.cc ${REBOUND_DIR}/interface.cc
 
 # Build AMUSE
-cd $(AMUSE_DIR)
+cd ${AMUSE_DIR}
 ./configure && \
 make framework && \
 make rebound.code
