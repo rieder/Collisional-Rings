0fdaf00cc9f09e8805305575abf0eb45dc87063e
Steven Rieder <steven@rieder.nl>
2017-04-03 16:32:55 +0900

    Use updated Amuse/Rebound, no more patches needed.

--- get_amuse.sh
+++ get_amuse.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Download and extract AMUSEa
-export GITHASH=4dccd5460fc37f0663307250ac3073ac91b49331
+export GITHASH=ba6280804d15248156e4eb923d5b0f6e1a710b31
 wget https://github.com/rieder/amuse/archive/${GITHASH}.zip && \
   unzip ${GITHASH}.zip && \
   mv amuse-${GITHASH} amuse && \
@@ -14,9 +14,6 @@ export REBOUND_DIR="${AMUSE_DIR}/src/amuse/community/rebound"
 # Patch rebound makefile to compile on my Macbook
 cd ${REBOUND_DIR}
 python download_http.py 
-cd ${SCRIPT_DIR}
-cp patches/rebound_Makefile.defs ${REBOUND_DIR}/src/rebound/src/Makefile.defs 
-cp patches/rebound_interface.cc ${REBOUND_DIR}/interface.cc
 
 # Build AMUSE
 cd ${AMUSE_DIR}
@@ -24,4 +21,4 @@ cd ${AMUSE_DIR}
 make framework && \
 make rebound.code
 
-cd ..
+cd ${SCRIPT_DIR}
