4b5345cff9645708363bdfaf08a22b9773611d54
Steven Rieder <steven@rieder.nl>
2017-04-18 12:14:00 +0900

    Update get_amuse.sh

--- get_amuse.sh
+++ get_amuse.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Download and extract AMUSEa
-export GITHASH=e1f50d55b9fa102eb6aa028a6d75062bffa5d196
+export GITHASH=79ec73d1ac2409c7564b927a5fa70b323d8362d0
 wget https://github.com/rieder/amuse/archive/${GITHASH}.zip && \
   unzip ${GITHASH}.zip && \
   mv amuse-${GITHASH} amuse && \
